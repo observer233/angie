ca1a4034ef5d4fd01e2f1f7366940483c24dbdee
柑霜 <2513554640@qq.com>
2026-03-17 21:11:30 +0800

    装修我的花园：修改了配色和标题

--- src/sample.ts
+++ src/sample.ts
@@ -1,15 +1,15 @@
 const author = {
-  name: `Angela`,
-  nickname: `angie`,
+  name: `柑霜`,
+  nickname: `Aloy`,
   tiktok: `https://www.tiktok.com/@indieteamofone`,
   ig: `https://www.instagram.com/indieteamofone/`,
   x: `https://x.com/indieteamofone`,
-  email: "mailto:anthonylannn@gmail.com",
+  email: "2513554640@qq.com",
 };
 
 const buy = {
   title: `See on Github`,
-  link: `https://github.com/anthonylan/angie`,
+  link: `https://github.com/observer233/angie`,
 };
 
 export { author, buy };
